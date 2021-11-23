444ed0bf737177d309b52ad57ff65092de6ffcc9
Bruno da Silveira Noremberg <94628573+89bruno@users.noreply.github.com>
2021-11-23 11:00:43 -0300

    Update Projeto 1 - Conversor de moedas.0.py
    Corrigindo para o padrão snake_case

--- Projeto 1 - Conversor de moedas.0.py
+++ Projeto 1 - Conversor de moedas.0.py
@@ -4,47 +4,47 @@ import requests
 from time import gmtime, strftime
 
 # Passo Inicial - Entrada dos valores
-Nome = input("Bem-vindo(a), qual seu nome?")
-Reais = (input(f"""Olá {Nome}, digite o valor em Reais (R$) que você deseja converter e precione enter: \n"""))
+nome = input("Bem-vindo(a), qual seu nome?")
+reais = (input(f"""Olá {nome}, digite o valor em Reais (R$) que você deseja converter e precione enter: \n"""))
 
 while True:
-    if not Reais.isnumeric():
-        Reais = input(f'{Reais} não é um valor válido, entre com um valor numérico inteiro:')
+    if not reais.isnumeric():
+        reais = input(f'{reais} não é um valor válido, entre com um valor numérico inteiro:')
         continue
     else:
-        print(f"""Ok, vamos lá! Convertendo R${Reais},00 para Dolar, Euro e Bitcoin\n""")
+        print(f"""Ok, vamos lá! Convertendo R${reais},00 para Dolar, Euro e Bitcoin\n""")
         break
 
 # Obtendo as Cotações:
-Moeda = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").json()
+moeda = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").json()
 
-Dolar = Moeda['USDBRL']
-CotDolar = float(Dolar["bid"])
+dolar = moeda['USDBRL']
+cot_dolar = float(dolar["bid"])
 
-Euro = Moeda['EURBRL']
-CotEuro = float(Euro["bid"])
+euro = Moeda['EURBRL']
+cot_euro = float(euro["bid"])
 
-Bitcoin = Moeda['BTCBRL']
-CotBit = float(Bitcoin["bid"])
+bitcoin = moeda['BTCBRL']
+cot_bit = float(bitcoin["bid"])
 
 # Convertendo os valores:
-Valor = float(Reais)
-ReaisToDolar = str(round((Valor / CotDolar),2)).replace('.',',')
+valor = float(reais)
+reais_para_dolar = str(round((valor / cot_dolar),2)).replace('.',',')
 
-ReaisToEuro = str(round((Valor / CotEuro),2)).replace('.',',')
+reais_para_euro = str(round((valor / cot_euro),2)).replace('.',',')
 
-ReaisToBit = (Valor / CotBit)
-if ReaisToBit >= 1:
-       NovoReaisToBit = str(round(ReaisToBit, 2)).replace('.', ',')
+reais_para_bit = (valor / cot_bit)
+if reais_para_bit >= 1:
+       novo_reais_para_bit = str(round(reais_para_bit, 2)).replace('.', ',')
 else:
-    NovoReaisToBit = str(round(ReaisToBit, 3)).replace('.', ',')
+    novo_reais_para_bit = str(round(reais_para_bit, 3)).replace('.', ',')
 
 # Imprimindo os resultados:
-Data = strftime("%d-%m-%y às %H:%M:%S", gmtime())
-print (f"""Na cotação de hoje*, R${Reais},00 equivalem a:
-    U$ {ReaisToDolar}
-    € {ReaisToEuro}
-    ₿ {NovoReaisToBit}
-*Cotação obtida em {Data}""")
+data = strftime("%d-%m-%y às %H:%M:%S", gmtime())
+print (f"""Na cotação de hoje*, R${reais},00 equivalem a:
+    U$ {reais_para_dolar}
+    € {reais_para_euro}
+    ₿ {novo_reais_para_bit}
+*Cotação obtida em {data}""")
 
 
